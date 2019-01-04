d0f11048d78f3d9c4cc1053b9a0e67c91f53c336
sitisofiyah <jarakostacity@gmail.com>
2019-01-04 17:42:40 +0700

    Add files via upload

---
+++ mengubah bilangan pecahan ke bilangan bulat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Memasukkan angka pecahan =";
+    cin>>a;
+
+    cout<<"Bilangan bulat ini adalah ="<<a<<endl;
+    return 0;
+}
